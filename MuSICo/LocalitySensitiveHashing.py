064f42b9172d2d645a8d5672968aabc5b78766ab
davidsbatista <dsbatista@gmail.com>
2015-10-06 16:48:50 +0100

    initial import

---
+++ MuSICo/LocalitySensitiveHashing.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "David S. Batista"
+__email__ = "dsbatista@inesc-id.pt"
+
+import numpy
+import sys
+import redis
+import pickle
+
+from collections import defaultdict
+from operator import itemgetter
+
+
+class LocalitySensitiveHashing:
+
+    n_sigs = None
+    n_bands = None
+    sigs_per_band = None
+    kNN = None
+    bands = []
+    minhash_instances = None
+
+    def __init__(self, n_bands, n_sigs, knn, use_redis):
+        if (n_sigs % n_bands) != 0:
+            print "Error! Number of hash signatures must be a multiple of number of bands"
+            sys.exit(0)
+        else:
+            self.n_sigs = n_sigs
+            self.n_bands = n_bands
+            self.sigs_per_band = n_sigs / n_bands
+            self.kNN = knn
+
+            for i in range(0, n_bands):
+                conn = redis.StrictRedis(host='localhost', port=6379, db=i)
+                self.bands.append(conn)
+            self.minhash_instances = redis.StrictRedis(host='localhost', port=6379, db=n_bands+1)
+
+    def create(self):
+        for i in range(0, self.n_bands):
+            self.bands.append(dict())
+
+    def index(self, relationship):
+        # generates a list of equaly sized chunks (arrays) from the min-hash array
+        chunked = [s for s in self.chunks(relationship.sigs, self.sigs_per_band)]
+        for i in range(0, len(chunked)):
+            sorted_chunk = numpy.sort(chunked[i])
+            self.bands[i].set(tuple(sorted_chunk), (relationship.rel_type, relationship.identifier))
+
+        # save the minh-hash signatures of the relationship
+        # allows for real Jaccardi calculation in classication
+        sigs_pickled = pickle.dumps(relationship.sigs)
+        self.minhash_instances.set(relationship.identifier, sigs_pickled)
+
+    @staticmethod
+    def chunks(l, n):
+        for i in xrange(0, len(l), n):
+            yield l[i:i + n]
+
+    def classify(self, relationship):
+        # generates a list of equaly sized chunks (arrays) from the min-hash array
+        chunked = [s for s in self.chunks(relationship.sigs, self.sigs_per_band)]
+        candidates = defaultdict(list)
+
+        for i in range(0, len(chunked)):
+            sorted_chunk = numpy.sort(chunked[i])
+            # TODO, redis_i, ..., redis_n
+            # .set(tuple(sorted_chunk)
+            if self.bands[i].get(tuple(sorted_chunk)):
+                value = eval(self.bands[i].get(tuple(sorted_chunk)))
+                rel_type, rel_id = value[0], value[1]
+                candidates[rel_type].append(rel_id)
+
+        # get each candidate full signatures
+        # stores candidate and approximate Jaccardi score
+        scores = []
+        for rel_type in candidates:
+            for rel_id in candidates[rel_type]:
+                # get candidate min-hash sigs
+                candidate_sigs = self.minhash_instances.get(rel_id)
+
+                # calculate aproximate Jaccardi Similarity using the full min-hash sigs
+                # logical XOR between two array of min-hash sigs:
+                #   - gives False if two elements are equal, True if two elements are different
+                #   - return array of boolean values (False,True)
+                # .sum() of the array of boolean gives the number of True, i.e., ones
+                #   - Jaccardi is number of equal signatures (False) over the total number of signatures
+                sigs = pickle.loads(candidate_sigs)
+                score = 1-numpy.logical_xor(relationship.sigs, sigs).sum() / float(self.n_sigs)
+                scores.append((rel_type, rel_id, score))
+
+        if len(scores) == 0:
+            return "no candidates"
+
+        else:
+            rel_sorted = sorted(scores, key=itemgetter(2), reverse=True)
+            # consider only the top kNN candidates
+            top_k = rel_sorted[:self.kNN]
+            output = dict()
+            # choose the most common
+            for x in top_k:
+                if x[0] in output:
+                    output[x[0]] += 1
+                else:
+                    output[x[0]] = 1
+
+            output_sorted = sorted(output, key=itemgetter(1), reverse=True)
+            return output_sorted[0]
---
+++ MuSICo/MinHashClassifier.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "David S. Batista"
+__email__ = "dsbatista@inesc-id.pt"
+
+import re
+import os
+import sys
+import codecs
+import pickle
+
+from nltk.data import load
+
+import MinHash
+from Sentence import Sentence, Relationship
+from FeatureExtractor import FeatureExtractor
+from LocalitySensitiveHashing import LocalitySensitiveHashing
+
+
+N_BANDS = 100
+N_SIGS = 800
+KNN = 7
+
+
+def classify_sentences(data_file, extractor, lsh):
+    """
+    receives a file with sentences where entities are identified
+    and for each classifies the existing relationships between the entities
+
+    :param data_file:
+    :param extractor:
+    :param lsh:
+    :return:
+    """
+    f_sentences = codecs.open(data_file, encoding='utf-8')
+    for line in f_sentences:
+        if len(line) > 1:
+            sentence = line.strip()
+            sentence = Sentence(sentence)
+            for rel in sentence.relationships:
+                if rel.arg1type is None and rel.arg2type is None:
+                    continue
+                else:
+                    # extract features/shingles
+                    features = extractor.extract_features(rel)
+                    shingles = features.getvalue().strip().split(' ')
+
+                    # calculate min-hash sigs
+                    sigs = MinHash.signature(shingles, N_SIGS)
+                    rel.sigs = sigs
+
+                    # find closest neighbours
+                    types = lsh.classify(rel)
+                    print rel.sentence.encode("utf8") + '\targ1:'+rel.ent1.encode("utf8") + '\targ2:ent2'+rel.ent2.encode("utf8") + '\t' + types.encode("utf8")
+
+    f_sentences.close()
+
+
+def load_training_relationships(data_file, extractor):
+    relationships = []
+    sentence = None
+    rel_id = 0
+    f_sentences = codecs.open(data_file, encoding='utf-8')
+    f_features = open('features.txt', 'w')
+
+    for line in f_sentences:
+        #sys.stdout.write('.')
+        if not re.match('^relation', line):
+            sentence = line.strip()
+        else:
+            rel_type = line.strip().split(':')[1]
+            rel = Relationship(sentence, None, None, None, None, None, None, None, rel_type, rel_id)
+
+            # extract features/shingles
+            features = extractor.extract_features(rel)
+            shingles = features.getvalue().strip().split(' ')
+
+            # write shingles to StringIO
+            f_features.write(rel_type + '\t')
+            for shingle in shingles:
+                f_features.write(shingle.encode("utf8") + ' ')
+            f_features.write('\n')
+
+            # calculate min-hash sigs
+            sigs = MinHash.signature(shingles, N_SIGS)
+            rel.sigs = sigs
+            rel.identifier = rel_id
+
+            rel_id += 1
+            relationships.append(rel)
+
+    f_sentences.close()
+    f_features.close()
+
+    return relationships
+
+
+def load_shingles(shingles_file):
+    """
+    Parses already extracted shingles from a file.
+    File format is: relaltionship_type \t shingle1 shingle2 shingle3 ... shingle_n
+    """
+    relationships = []
+    rel_identifier = 0
+    f_shingles = codecs.open(shingles_file, encoding='utf-8')
+
+    for line in f_shingles:
+        sys.stdout.write('.')
+        rel_type, shingles_string = line.split('\t')
+        shingles = shingles_string.strip().split(' ')
+
+        # calculate min-hash sigs
+        sigs = MinHash.signature(shingles, N_SIGS)
+
+        rel = Relationship(None, None, None, None, None, None, None, None, rel_type, rel_identifier)
+        rel.sigs = sigs
+        rel.identifier = rel_identifier
+        relationships.append(rel)
+        rel_identifier += 1
+    
+    f_shingles.close()
+
+    return relationships
+
+
+def main():
+
+    ###########################
+    # CLASSIFY A NEW SENTENCE
+    ###########################
+    # argv[1] - true
+    # argv[2] - bands file
+    # argv[3] - dict (sigs->sentence_id) file
+    # argv[4] - sentence to classify
+
+    if sys.argv[1] == 'true':
+
+        print "Loading PoS tagger"
+        model = open('postagger/datasets/cintil-reduced-tagset.pkl', "rb")
+        pos_tagger = pickle.load(model)
+        model.close()
+
+        print "Loading verbs conjugations"
+        f_verbs = open('verbs/verbs_conj.pkl', "rb")
+        verbs = pickle.load(f_verbs)
+        f_verbs.close()
+
+        extractor = FeatureExtractor(pos_tagger, verbs)
+        lsh = LocalitySensitiveHashing(N_BANDS, N_SIGS, KNN, USE_REDIS)
+        
+        # read sentences from file
+        # create a relationship object
+        # extract features/shingles and calculate min-hash sigs
+        classify_sentences(sys.argv[2], extractor, lsh)
+
+    ############################
+    # INDEX TRAINING INSTANCES #
+    ############################
+    # argv[1] - false
+    # argv[2] - training data file (example: train_data.txt)
+
+    elif sys.argv[1] == 'false':
+
+        # calculate min-hash sigs (from already extracted shingles) index in bands
+        if os.path.isfile("features.txt"):
+            print "Calculating min-hash sigs from features.txt file"
+            relationships = load_shingles('features.txt')
+            print "\n"
+            print "Indexing ", len(relationships), "relationships"
+            print "MinHash Signatures: ", N_SIGS
+            print "Bands             : ", N_BANDS
+            print "Sigs per Band     : ", N_SIGS / N_BANDS
+            lsh = LocalitySensitiveHashing(N_BANDS, N_SIGS, KNN, USE_REDIS)
+            lsh.create()
+            for r in relationships:
+                lsh.index(r)
+
+        # load sentences, extract features, calculate min-hash sigs, index in bands
+        else:
+            print "Loading PoS tagger"
+            tagger = load('taggers/maxent_treebank_pos_tagger/english.pickle')
+            extractor = FeatureExtractor(tagger)
+
+            print "Extracting features from training data and calculating min-hash sigs"
+            relationships = load_training_relationships(sys.argv[2], extractor)
+            print "\n"
+            print "Indexing ", len(relationships), "relationships"
+            print "MinHash Signatures: ", N_SIGS
+            print "Bands             : ", N_BANDS
+            lsh = LocalitySensitiveHashing(N_BANDS, N_SIGS, KNN, USE_REDIS)
+            lsh.create()
+            for r in relationships:
+                lsh.index(r)
+
+if __name__ == "__main__":
+    main()
