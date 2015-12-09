255926d0800f129b1448c48157edb9bf1303d050
davidsbatista <dsbatista@gmail.com>
2015-12-09 09:32:09 +0000

    batch-script updated

--- batch-evaluation.sh
+++ batch-evaluation.sh
@@ -10,18 +10,35 @@ KNN=(1 3 5 7)
 for s in 100 200 300 400 500 600; do
     for b in 25 50 75 100 150; do
         for k in 1 3 5 7; do
-            echo $s $b $k
+            DIR=${s}.${b}.${k}
+            echo ${DIR}
+            mkdir ${DIR}
+            cd ${DIR}
+            # clear REDIS tables
+            redis-cli flushall
+
+            # link to file with generated features
+            ln -s ../features.txt
+
+            # index training data
+            ../MinHashClassifier.py index ../training_data.txt ${N_BANDS} ${N_SIGS}
+
+            # classifiy training data
+            ../MinHashClassifier.py classify2 ../set_b_shingles.txt ${N_BANDS} ${N_SIGS} ${KNN}
+
+            # create a separate file for each relationship type
+            ../musico2evaluation.sh
+
+            # link to files needed by evaluation framework
+            for i in ../superset_*; do ln -vs ${i}; done
+            for i in ../*high_pmi*; do ln -sv ${i}; done
+
+            # evaluate each relationship type
+            ../evaluate-musico.sh
+
+            # output results to single file
+            for i in *_results.txt; do echo ${i}; tail -n 5 ${i}; done >> results.txt
+            cd ..
         done
     done
 done
-
-# make dir
-# change dir
-
-# Index training data
-# TODO: accept BANDS and SIGS from command line
-#./MinHashClassifier.py index training_data.txt
-
-# Classifiy training data:
-#./MinHashClassifier.py classifiy2 <set_b_matched.txt>
-
